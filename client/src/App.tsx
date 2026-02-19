e4dd7d0c1f8ce677af255668740e9cec146e1a77
J Rahul <rahul.j@jmangroup.com>
2026-02-19 15:26:40 +0530

    Add sidebar navigation and course list UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css'
-
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import HomePage from "./pages/HomePage";
+import AllCoursesPage from "./pages/AllCoursesPage";
 
-function App() {
-  return <HomePage />;
-}
+const App: React.FC = () => {
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+
+  return (
+    <BrowserRouter>
+      <Navbar onMenuClick={() => setSidebarOpen(true)} />
+      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+
+      <main className="w-full">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/courses/:section" element={<AllCoursesPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
 
 export default App;
-
